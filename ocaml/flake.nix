4209fe7fdc97dff027176f237c4693fa99249256
Adam Cholewiński <me@adamchol.com>
2025-07-22 19:45:03 +0200

    ocaml: bump flake inputs, add manual reload to direnv

--- ocaml/flake.nix
+++ ocaml/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/81bbc0eb0b178d014b95fc769f514bedb26a6127";
+    nixpkgs.url = "github:nixos/nixpkgs/58154087ab13af588093c819585737b833064803";
     flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b";
-    ocaml-overlays.url = "github:nix-ocaml/nix-overlays/baeae17459e1150426334f7be3bfe83ec0d752c9";
+    ocaml-overlays.url = "github:nix-ocaml/nix-overlays/828cc4d2765c6446afa4743678d391152af7e6c5";
   };
 
   outputs = {
